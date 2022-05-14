9b0d285750952c966c149243141150ab269f83a9
Oliver <71002514+zlbyapi@users.noreply.github.com>
2022-05-14 19:54:44 +0200

    initial?

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let url = 'https://minotar.net/helm/';
+	let name = 'e0';
+</script>
+
+<main>
+	<p>Unicode Copy and Paste</p>
+	<input bind:value="{name}" class="input" type="text" placeholder="unicode (example: e067)"> <br>
+    <iframe src="https://www.fileformat.info/info/unicode/char/{name}/browsertest.htm" width="1000px" height="1000" frameborder="0"></iframe>
+</main>
+
+<style>
+
+</style>
