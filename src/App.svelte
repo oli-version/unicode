09a3f0fe80a8c3b9e483665b6546a78017b311ad
Oliver <hulley.oliver@gmail.com>
2022-05-14 20:07:03 +0200

    Made look better (kind of)

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script>
-	let url = 'https://minotar.net/helm/';
 	let name = '';
 </script>
 
 <main>
-	<p>Unicode Copy and Paste</p>
+	<h1>Oli's unicode copy&paste</h1>
+	<p>Just enter the unicode below and it will automatically update (Example: E067)</p>
+	<p>You can view the very messy source code <a target="_blank" href="https://github.com/oli-version/unicode">here</a>.</p>
 	<input bind:value="{name}" class="input" type="text" placeholder="unicode (example: e067)"> <br>
-    <iframe src="https://www.fileformat.info/info/unicode/char/{name}/browsertest.htm" width="1000px" height="1000" frameborder="0"></iframe>
+    <iframe src="https://www.fileformat.info/info/unicode/char/{name}/browsertest.htm" width="1000px" height="550" frameborder="0" style="border-radius: 10px;"></iframe>
 </main>
 
 <style>
